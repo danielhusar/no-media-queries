1a2086905d58be155ac956420bd74591a1022e43
Daniel Husar <dano.husar@gmail.com>
2014-05-12 13:02:49 +0100

    Indentation

--- cli.js
+++ cli.js
@@ -6,23 +6,23 @@ var nmq = require('./index');
 var input = process.argv[2];
 
 function stdin(cb) {
-	var ret = '';
-	process.stdin.on('data', function (data) { ret += data });
-	process.stdin.on('end', cb.bind(null, ret));
+  var ret = '';
+  process.stdin.on('data', function (data) { ret += data });
+  process.stdin.on('end', cb.bind(null, ret));
 }
 
 function help() {
-	console.log(pkg.description);
-	console.log('');
-	console.log('Usage:');
-	console.log('  $ nmq <file> > <new-file>');
-	console.log('');
-	console.log('Example:');
+  console.log(pkg.description);
+  console.log('');
+  console.log('Usage:');
+  console.log('  $ nmq <file> > <new-file>');
+  console.log('');
+  console.log('Example:');
   console.log('  $ nmq responsive.css > non-responsive.css');
   console.log('');
   console.log('Custom options:');
   console.log('Non Media Quaries options with one dash "-", rewok options with two dash "--"');
-	console.log('  $ nmq responsive.css > non-responsive.css -width=200px --compress=true');
+  console.log('  $ nmq responsive.css > non-responsive.css -width=200px --compress=true');
 }
 
 function init(data) {
@@ -41,17 +41,15 @@ function init(data) {
   process.stdout.write(nmq(data, mqOpts, reOpts));
 }
 
-
 if (process.argv.indexOf('-h') !== -1 || process.argv.indexOf('--help') !== -1) {
-	help();
-	return;
+  help();
+  return;
 }
 
 if (process.argv.indexOf('-v') !== -1 || process.argv.indexOf('--version') !== -1) {
-	console.log(pkg.version);
-	return;
+  console.log(pkg.version);
+  return;
 }
-
 
 if (process.stdin.isTTY) {
   if (!input) {
